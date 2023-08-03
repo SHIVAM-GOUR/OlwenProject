2ca270285521b2e1de959eb2a2d6bc25a33e8cef
shivamgourteams <shivam@turing-labs.in>
2023-08-03 14:52:56 +0530

    convert to Go4Agri

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import Footer from '../components/footer/Footer'
-import MetaHead from '../components/header/MetaHead'
-import HomePage from '../components/home/HomePage'
-import HomeComponent from '../components/navbar/Navbar'
-import Navbar from '../components/navbar/Navbar'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+// import Footer from "../components/footer/Footer";
+// import MetaHead from "../components/header/MetaHead";
+// import HomePage from "../components/home/HomePage";
+// import HomeComponent from "../components/navbar/Navbar";
+// import Navbar from "../components/navbar/Navbar";
+// import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  // Testing Push
   return (
     <div>
-      <MetaHead title="Olwen lifesciences" />
-      <Navbar/>
-      <HomePage />
+      <Head>
+        <title>Go4Agri</title>
+        <meta
+          name="description"
+          content="Go4Agri"
+        />
+      </Head>
+      <h1>Go4Agri</h1>
+      {/* CODE OF OLWEN */}
+      {/* <MetaHead title="Olwen lifesciences" /> */}
+      {/* <Navbar/>
+      <HomePage /> */}
     </div>
-  )
+  );
 }
