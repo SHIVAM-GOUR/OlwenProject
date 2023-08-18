0d44d5132ed3b0155c3b18f741b11b49e27452a9
shivamgourteams <shivam@turing-labs.in>
2023-08-18 19:16:03 +0530

    logo update in contact section update

--- components/Go4Agri/GoAgriHome.jsx
+++ components/Go4Agri/GoAgriHome.jsx
@@ -4,6 +4,8 @@ import { Button, useMediaQuery } from "@mui/material";
 import ImageWithText from "./ImageWithText";
 import poster from "../../public/images/agriImage.jpg";
 import Image from "next/image";
+import LOGO from "../../public/logo/GO4AGRI-PNG-LOGO.png";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const GoAgriHome = () => {
   const isMobile = useMediaQuery("max-width:600px");
@@ -20,7 +22,7 @@ const GoAgriHome = () => {
   return (
     <div className={styles.container}>
       <div className={styles.navbar}>
-        <p className={styles.logo}>Go4Agri</p>
+        <Image src={LOGO} width={100} height={100} alt="go4agri logo" />
         <Button
           variant="contained"
           className={styles.button}
@@ -29,12 +31,21 @@ const GoAgriHome = () => {
           Contact
         </Button>
       </div>
-      {/* <Image src={poster} /> */}
-      <ImageWithText
-        // imageSrc={image}
-        text="Welcome to Go4Agri Certifications!"
-      />
-      <div className={styles.formBox} id="form">
+      <ImageWithText text="Welcome to Go4Agri Certifications!" />
+      <div className={styles.contactContainer}>
+        <div className={styles.contactBox} id="form">
+          <p className={styles.contactPara}>
+            We are happy to connect with you for any queries. <br /> Please
+            contact on given mail below :
+          </p>
+          <div className={styles.contactMail}>
+            <MailOutlineIcon className={styles.mailIcon} />
+            <a href="mailto:info@go4agri.co.in" className={styles.mailText}>Info@go4agri.co.in</a>
+          </div>
+        </div>
+      </div>
+      {/* GOOGLE FORM IMBEDED CODE */}
+      {/* <div className={styles.formBox} id="form">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSdpZKOTR8Ei-jw74RsxVRXm644BFP1o7XchkilNR-4-3j8zOg/viewform?embedded=true"
           width="330"
@@ -47,7 +58,7 @@ const GoAgriHome = () => {
         >
           Loading…
         </iframe>
-      </div>
+      </div> */}
       <div className={styles.footer}>
         <p>Copyright 2023, Go4Agri All rights reserved.</p>
       </div>
